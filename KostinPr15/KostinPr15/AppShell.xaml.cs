a2c7fe55cd0df6c30ef4efc3b4e83d8e255bc009
Игорь <gamora1313@mail.ru>
2022-04-18 00:39:01 +0300

    Добавьте файлы проекта.

---
+++ KostinPr15/KostinPr15/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using KostinPr15.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace KostinPr15
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
